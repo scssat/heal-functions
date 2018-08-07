d854434081f19adf78a904c45666b170be205f30
Svein Soermo <scssat@gmail.com>
2018-08-07 06:17:02 +0200

    Forum Aggregation completed

--- src/forumaggregate/index.ts
+++ src/forumaggregate/index.ts
@@ -4,44 +4,49 @@ import * as mbhCollection from '../collections';
 
 export const aggregateComments = functions.firestore
   .document(`${mbhCollection.MBH_FORUM_COMMENTS}/{commentId}`)
-  .onWrite((snap, event) => {
-    const comment = snap.data();
+  .onWrite((change, event) => {
+    const comment = change.after.data();
 
     // ref to the parent document
     const postRef = admin
       .firestore()
-      .collection(`${mbhCollection.MBH_FORUM}`)
-      .doc(comment.postId);
+      .collection('forumposts')
+      .doc(comment.forumPostId);
 
     const commentRef = admin
       .firestore()
       .collection(`${mbhCollection.MBH_FORUM_COMMENTS}`);
 
+    console.log(comment.forumPostId);
     // get all comments and aggregate
-    return commentRef
+    commentRef
+      .where('forumPostId', '==', comment.forumPostId)
       .orderBy('created', 'desc')
       .get()
       .then(querySnapshot => {
         // get the total comment count
-        const commentCount = querySnapshot.size;
+        const numberOfComments = querySnapshot.size + 1;
 
-        const recentComments = [];
+        let recentComments = [];
 
-        // add data from the 20 most recent comments to the array
+        // add data from the 10 most recent comments to the array
         querySnapshot.forEach(doc => {
           recentComments.push(doc.data());
         });
 
-        recentComments.splice(20);
+        recentComments = [...recentComments.slice(0, 9)];
 
         // record last comment timestamp
-        const lastActivity = recentComments[0].created;
+        const lastUpdate = recentComments[0].created;
 
         // data to update on the document
-        const data = { commentCount, recentComments, lastActivity };
-
+        const data = { numberOfComments, recentComments, lastUpdate };
+        console.log('Number of comments:', numberOfComments);
         // run update
-        return postRef.update(data);
+        return postRef
+          .update(data)
+          .catch(err => console.log('Error when writing to post', err));
       })
-      .catch(err => console.log('Error when writing to post', err));
+      .catch(err => console.log('Error when reading comments', err));
+    return null;
   });
